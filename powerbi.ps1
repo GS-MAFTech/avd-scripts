4b098c98c1fb7e1dcd62a364b6f039bbcb802229
mennatallah-abdallah-e <91662825+mennatallah-abdallah-e@users.noreply.github.com>
2024-02-22 14:07:39 +0200

    Create powerbi.ps1

---
+++ powerbi.ps1
@@ -0,0 +1,18 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$A = ""
+
+## Initial link for the product
+## "https://www.microsoft.com/en-us/download/details.aspx?id=58494"
+
+## Download Page for PowerBI refered by the Download button of the previous page
+$DownloadPowerBIUri = "https://www.microsoft.com/en-us/download/confirmation.aspx?id=58494"
+
+## Get the Page content
+$A = Invoke-WebRequest -Uri $DownloadPowerBIUri -UseBasicParsing
+
+## Find the first x64 link and download the file to c:\temp
+Invoke-WebRequest -Uri ( ($A.Links | ? {$_.href -like "*PBIDesktopSetup_x64.exe*" }) | Select -First 1 ).href -OutFile c:\Temp\PBIDesktopSetup_x64.exe
+
+$process = Start-Process c:\Temp\PBIDesktopSetup_x64.exe -ArgumentList -silent -passive -quiet ACCEPT_EULA=1
+Write-Host "INFO:powerbi install finished."
+
